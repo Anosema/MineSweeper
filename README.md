13092e7947d253f5e560c09ecf423d45747ffc7a
Anosema <lordchips@outlook.fr>
2020-10-15 19:16:47 +0200

    added custom maps

--- README.md
+++ README.md
@@ -4,8 +4,25 @@ Python 3.8.5 Minesweeper for terminal (Linux and Windows) by Anosema
 ## Starting With Minesweeper
 - `git clone https://github.com/Anosema/MineSweeper`
 - `cd MineSweeper`
-- `python minesweeper.py [difficulty 1-3]` or `python3 minesweeper.py [difficulty 1-3]`
 
+### Usage
+	- `python minesweeper.py [difficulty 1-3]`
+	  or
+	  `python3 minesweeper.py [difficulty 1-3]`
+	- `python  minesweeper custom [x] [y] [number of bombs]`
+	  or
+	  `python3 minesweeper custom [x] [y] [number of bombs]`
+
+
+
+
+
+
+
+
+
+
+Usage :\n    -`python  minesweeper.py [difficulty 1-3]`\n    -`python3 minesweeper.py [difficulty 1-3]`\n    or\n    -`python  minesweeper custom [x] [y] [number of bombs]`\n    -`python3 minesweeper custom [x] [y] [number of bombs]`"
 ## Playing Minesweeper
 - You have 4 different commands :
 	- `x,y` where x and y are the coordinates of the cell you want to uncover.
--- minesweeper.py
+++ minesweeper.py
@@ -1,17 +1,37 @@
 import random, os, sys, time
 
-color_Yel = '\x1b[1;33;40m'
-color_Ora = '\x1b[0;33;40m'
-color_Lim = '\x1b[0;32;40m'
-color_Gre = '\x1b[1;32;40m'
-color_Red = '\x1b[0;31;40m'
-color_Blu = '\x1b[0;34;40m'
-color_Whi = '\x1b[1;37;40m'
-color_Pur = '\x1b[1;35;40m'
-color_End = '\x1b[0m'
+embed_end           = '\x1b[0m'
+embed_bold          = '\x1b[1m'
+embed_underline     = '\x1b[4m'
+embed_blink         = '\x1b[5m'
+embed_black         = '\x1b[30m'
+embed_red           = '\x1b[31m'
+embed_green         = '\x1b[32m'
+embed_yellow        = '\x1b[33m'
+embed_blue          = '\x1b[34m'
+embed_magenta       = '\x1b[35m'
+embed_cyan          = '\x1b[36m'
+embed_white         = '\x1b[37m'
+embed_light_gray    = '\x1b[90m'
+embed_light_red     = '\x1b[91m'
+embed_light_green   = '\x1b[92m'
+embed_light_yellow  = '\x1b[93m'
+embed_light_blue    = '\x1b[94m'
+embed_light_magenta = '\x1b[95m'
+embed_light_cyan    = '\x1b[96m'
+embed_light_white   = '\x1b[97m'
 
-blink_Sta = '\033[5m'
-blink_End = '\033[0m'
+
+
+
+
+
+
+
+
+
+
+
 
 class Cell():
 	def __init__(self, x, y):
@@ -32,82 +52,89 @@ class Cell():
 				self.marked = False
 				self.state  = "#"
 			else:
-				self.marked = True
-				self.state  = f"{color_Red}?{color_End}"
+				if self.state == "#":
+					self.marked = True
+					self.state  = f"{embed_red}?{embed_end}"
 			return True
 		elif mark == "False":
-			if self.bombed:
-				self.state  = f"{color_Red}X{color_End}"
-				return False
-			else:
-				self.checked   = True
-				adj_cells      =[(self.y-1, self.x-1), (self.y-1, self.x), (self.y-1, self.x+1),
-							     (self.y  , self.x-1),                     (self.y  , self.x+1),
-							     (self.y+1, self.x-1), (self.y+1, self.x), (self.y+1, self.x+1)]
-				self.near_bomb = 0
-				
-				# Counting near bombs
-				for cos in adj_cells:
-					if 0 <= cos[1] < width and 0 <= cos[0] < height and grid[cos[0]][cos[1]].bombed == True:
-						self.near_bomb += 1
-	
-				# Showing near bombs
-				if self.near_bomb == 0:
-					self.state    = f"{color_Gre}0{color_End}"
-					# Reveal adjacents cells when selected a 0 cell
-					for cos in adj_cells:
-						if 0 <= cos[1] < width and 0 <= cos[0] < height and grid[cos[0]][cos[1]].checked == False:
-							grid[cos[0]][cos[1]].check()
-				elif self.near_bomb == 1:
-					self.state = f"{color_Yel}{self.near_bomb}{color_End}"
-				elif 2 <= self.near_bomb <= 3:
-					self.state = f"{color_Ora}{self.near_bomb}{color_End}"
-				elif 3 < self.near_bomb:
-					self.state = f"{color_Pur}{self.near_bomb}{color_End}"
-				return True
+			if self.state == "#":
+				if self.marked == False:
+					if self.bombed:
+						self.state  = f"{embed_red}X{embed_end}"
+						return False
+					else:
+						self.checked   = True
+						adj_cells      =[(self.y-1, self.x-1), (self.y-1, self.x), (self.y-1, self.x+1),
+									     (self.y  , self.x-1),                     (self.y  , self.x+1),
+									     (self.y+1, self.x-1), (self.y+1, self.x), (self.y+1, self.x+1)]
+						self.near_bomb = 0
+						
+						# Counting near bombs
+						for cos in adj_cells:
+							if 0 <= cos[1] < width and 0 <= cos[0] < height and grid[cos[0]][cos[1]].bombed == True:
+								self.near_bomb += 1
+			
+						# Showing near bombs
+						if self.near_bomb == 0:
+							self.state    = f"{embed_green}0{embed_end}"
+							# Reveal adjacents cells when selected a 0 cell
+							for cos in adj_cells:
+								if 0 <= cos[1] < width and 0 <= cos[0] < height and grid[cos[0]][cos[1]].checked == False:
+									grid[cos[0]][cos[1]].check()
+						elif self.near_bomb == 1:
+							self.state = f"{embed_light_yellow}{self.near_bomb}{embed_end}"
+						elif 2 <= self.near_bomb <= 3:
+							self.state = f"{embed_yellow}{self.near_bomb}{embed_end}"
+						elif 3 < self.near_bomb:
+							self.state = f"{embed_magenta}{self.near_bomb}{embed_end}"
+						return True
+		return True
 
-def create_grid(opt):
+# Create minesweeper
+def create_grid(opt, custom=False):
 	global width, height
-
-	# Selecting difficulty
-	if   int(opt[1]) == 1:
+	if custom:
+		if int(opt[2]) <= 10 and int(opt[3]) <= 10:
+			width,height,bomb_number = int(opt[2]), int(opt[3]), int(opt[4])
+# Selecting difficulty
+	elif int(opt[1]) == 1:
 		width,height,bomb_number = 5 , 5 , 5
 	elif int(opt[1]) == 2:
 		width,height,bomb_number = 7 , 7 , 10
 	elif int(opt[1]) == 3:
 		width,height,bomb_number = 10, 10, 15
 
-	# Adding cells
+# Adding cells
 	for y in range(height):
 		grid.append([Cell(x, y) for x in range(width)])
 
-	# Adding bombs
+# Adding bombs
 	for i in range(bomb_number):
 		random.choice(random.choice(grid)).bomb()
-	# solve()
+# solve()
 	play()
 
 def show_grid(finish=False):
 	global width, height
 
 	# Showing frame
-	print(f"{color_Blu}╔{'═'*(2 * width + 5)}╗{color_End}")
-	print(f"{color_Blu}║{color_End}{' ' * (width - 3)}{color_Lim}Minesweeper{color_End}{' '* (width - 3)}{color_Blu}║{color_End}")
-	print(f"{color_Blu}╠{'═'*(2 * width + 5)}╣{color_End}")
+	print(f"{embed_blue}╔{'═'*(2 * width + 5)}╗{embed_end}")
+	print(f"{embed_blue}║{embed_end}{' ' * (width - 3)}{embed_light_green}Minesweeper{embed_end}{' '* (width - 3)}{embed_blue}║{embed_end}")
+	print(f"{embed_blue}╠{'═'*(2 * width + 5)}╣{embed_end}")
 
 	# Showing columns number
-	print(f"{color_Blu}║   {''.join([f'{x} ' for x in range(width)])}  ║{color_End}")
+	print(f"{embed_blue}║   {''.join([f'{x} ' for x in range(width)])}  ║{embed_end}")
 	# Showing rows
 	for y in range(height):
-		print(f"{color_Blu}║ {y}{color_End} {''.join([f'{cell.state} ' for cell in grid[y]])}{color_Blu}{y} ║{color_End}")
+		print(f"{embed_blue}║ {y}{embed_end} {''.join([f'{cell.state} ' for cell in grid[y]])}{embed_blue}{y} ║{embed_end}")
 	# Showing columns number
-	print(f"{color_Blu}║   {''.join([f'{x} ' for x in range(width)])}  ║{color_End}")
+	print(f"{embed_blue}║   {''.join([f'{x} ' for x in range(width)])}  ║{embed_end}")
 
 	# Finishing frame
 	if finish:
-		print(f"{color_Blu}╠{'═'*(2 * width + 5)}╣{color_End}")
+		print(f"{embed_blue}╠{'═'*(2 * width + 5)}╣{embed_end}")
 	else:
-		print(f"{color_Blu}╚{'═'*(2 * width + 5)}╝{color_End}")
+		print(f"{embed_blue}╚{'═'*(2 * width + 5)}╝{embed_end}")
 
 def play():
 	to = time.time()
@@ -116,8 +143,8 @@ def play():
 	help()
 	show_grid()
 	while playing:
-		choice = input(f"Enter a couple 'x,y'\n>>> {color_Whi}").split(",")
-		print(color_End)
+		choice = input(f"Enter a couple 'x,y'\n>>> {embed_white}").split(",")
+		print(embed_end)
 		try:
 			if 0 <= int(choice[0]) < width and 0 <= int(choice[1]) < height:
 				try:
@@ -141,11 +168,11 @@ def play():
 	solve(True)
 	total_time = (int(10*(time.time() - to)))/10
 	if win:
-		print(f"{color_Blu}║{color_End}{' ' * (width - 2)}{blink_Sta}You Won !{blink_End}{' ' * (width - 2)}{color_Blu}║{color_End}")
+		print(f"{embed_blue}║{embed_end}{' ' * (width - 2)}{embed_blink}You Won !{embed_end}{' ' * (width - 2)}{embed_blue}║{embed_end}")
 	else:
-		print(f"{color_Blu}║{color_End}{' ' * (width - 2)}{blink_Sta}Game Over{blink_End}{' ' * (width - 2)}{color_Blu}║{color_End}")
-	print(f"{color_Blu}║{color_End} Time: {' '*((2 * width - 4)-len(str(total_time)))}{blink_Sta}{total_time}{blink_End}s {color_Blu}║{color_End}")
-	print(f"{color_Blu}╚{'═'*(2 * width + 5)}╝{color_End}")
+		print(f"{embed_blue}║{embed_end}{' ' * (width - 2)}{embed_blink}Game Over{embed_end}{' ' * (width - 2)}{embed_blue}║{embed_end}")
+	print(f"{embed_blue}║{embed_end} Time: {' '*((2 * width - 4)-len(str(total_time)))}{embed_blink}{total_time}s{embed_end} {embed_blue}║{embed_end}")
+	print(f"{embed_blue}╚{'═'*(2 * width + 5)}╝{embed_end}")
 
 
 def finalcheck():
@@ -168,11 +195,11 @@ def help():
 	print(
 f"""╔{"═"*96}╗
 ║  Help:{" "*89}║
-║ - To check a cell, enter the x and the y of the cell (for example "{color_Whi}1,2{color_End}" will check{" "*13}║
+║ - To check a cell, enter the x and the y of the cell (for example "{embed_white}1,2{embed_end}" will check{" "*13}║
 ║ the cell on the second row and third column){" "*51}║
-║ - To mark a cell, enter the x and the y of the cell followed by ",mark" (for example "{color_Whi}1,2,mark{color_End}"║
+║ - To mark a cell, enter the x and the y of the cell followed by ",mark" (for example "{embed_white}1,2,mark{embed_end}"║
 ║ will put a "?" at the place of the cell){" "*55}║
-║ - To show this help page, just enter "{color_Whi}help{color_End}\"{" "*52}║
+║ - To show this help page, just enter "{embed_white}help{embed_end}\"{" "*52}║
 ╚{"═"*96}╝""")
 	time.sleep(5)
 	os.system('clear')
@@ -184,8 +211,15 @@ def solve(finish=False):
 			grid[x][y].check()
 	show_grid(finish)
 
-if len(sys.argv)==2:
+if len(sys.argv) == 2:
 	grid = []
 	create_grid(sys.argv)
+
+elif len(sys.argv) == 5:
+	if sys.argv[1] != 'custom':
+		grid = []
+		create_grid(sys.argv, True)
+	else:
+		print("Usage :\n    -`python  minesweeper.py [difficulty 1-3]`\n    -`python3 minesweeper.py [difficulty 1-3]`\n    or\n    -`python  minesweeper custom [x] [y] [number of bombs]`\n    -`python3 minesweeper custom [x] [y] [number of bombs]`")
 else:
-	print("Usage : python3 mine.py [difficulty : 1-3]")
+	print("Usage :\n    -`python  minesweeper.py [difficulty 1-3]`\n    -`python3 minesweeper.py [difficulty 1-3]`\n    or\n    -`python  minesweeper custom [x] [y] [number of bombs]`\n    -`python3 minesweeper custom [x] [y] [number of bombs]`")
